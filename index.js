982fe4d7263d6a87b7661af84de57bdd9969721b
Nakkeez <markus.halminen@gmail.com>
2023-08-31 14:47:29 +0300

    Implement Auth0 login

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-mongoose.set('strictQuery', false);
+const { auth, requiresAuth } = require('express-openid-connect')
 
+const config = {
+  authRequired: process.env.AUTH_REQUIRED,
+  auth0Logout: process.env.AUTH0_LOGOUT,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.SECRET
+}
+
+app.use(auth(config))
 app.use(cors())
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname + '/client')));
 
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
@@ -34,29 +45,36 @@ const eventSchema = new mongoose.Schema({
 // Model
 const Event = mongoose.model('Event', eventSchema, 'events')
 
+// Login
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html')
+  if (request.oidc.isAuthenticated()) {
+    console.log(request.oidc.isAuthenticated())
+    response.sendFile(__dirname + '/index.html')
+  }
+  else {
+    response.redirect(__dirname + '/login');
+  }
 })
 
-// GET, POST, DEL and PUT routes
-app.get('/events', async (request, response) => {
+// GET, POST, DELETE and PUT routes
+app.get('/events', requiresAuth(), async (request, response) => {
   const events = await Event.find({})
   response.json(events)
 })
 
-app.post('/events', async (request, response) => {
+app.post('/events', requiresAuth(), async (request, response) => {
   const event = Event(request.body)
   const savedEvent = await event.save()
   response.json(savedEvent)  
 })
 
-app.delete('/events/:id', async (request, response) => {
+app.delete('/events/:id', requiresAuth(), async (request, response) => {
   const deletedEvent = await Event.findByIdAndRemove(request.params.id)
   if (deletedEvent) response.json(deletedEvent)
   else response.status(404).end()
 })
 
-app.put('/events/:id', async (request, response) => {
+app.put('/events/:id', requiresAuth(), async (request, response) => {
   const { mark } = request.body
   const updatedEvent = await Event.findByIdAndUpdate(request.params.id, { mark: mark })
   if (updatedEvent) {
